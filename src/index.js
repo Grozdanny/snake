08ba8fb88837a6400de7320d47172a449d809a87
Grozdanny <marcinzur277@gmail.com>
2021-10-12 12:28:47 +0000

    snAke HAHha

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import react from "react";
 import React from "react";
 import ReactDOM from "react-dom";
+
+class tile extends react.Component {
+
+}
+class grid extends react.component {
+
+}
+class game extends react.component {
+
+}
 
 ReactDOM.render(<Game />, document.getElementById("root"));
