d0988ca1460dc4d8b9b44d97d72c0ca9c5fe864e
Grozdanny <marcinzur277@gmail.com>
2021-10-13 11:40:09 +0000

    Grid

--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
-import react from "react";
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
-class Tile extends react.Component {
-
-}
-class Grid extends react.component {
-  render() {
-    const grid = Array(7).fill(Array(7).fill(null));
+class Tile extends React.Component{
+  constructor(props){
+      super(props);
+      
   }
-  
+  render(){
+      return <div className="tile">{`${this.props.X}, ${this.props.Y}`}</div>
+  }
 }
-class Game extends react.component {
+
+class Grid extends React.Component{
+  constructor(props){
+      super(props);
+  }
+
+  render() {
+
+      const ba = Array(7).fill(Array(7).fill(null)); 
+          
+          return (
+              <div className="xGrid">
+                  {ba.map((gridY,yIndex) => {
+                      return(
+                      <div className="yGrid">
+                          {gridY.map((gridX,xIndex) => {
+                             return <Tile X={xIndex} Y={yIndex} />
+                          })}
+                      </div>
+                      )
+                  }
+                  )}
+              </div>
+          )              
+  }
 
 }
+
+class Game extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            gridSize:[7, 7],
+            snake:[],
+            food:[],
+        }
+    }
+
+    render () {
+        return (
+                <Grid 
+                    grid={this.props.ba}
+                />
+               )
+    } 
+
+}
+
+
+
+
 
 ReactDOM.render(<Game />, document.getElementById("root"));
